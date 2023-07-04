4cd677c998d00d56c922fb9bc7284a3b655c290d
esamet <samete354@gmail.com>
2023-07-04 17:35:53 +0300

    fixed missing .h file

---
+++ diffdrive_arduino/include/diffdrive_arduino/diffdrive_arduino.h
@@ -0,0 +1,59 @@
+#ifndef DIFFDRIVE_ARDUINO_REAL_ROBOT_H
+#define DIFFDRIVE_ARDUINO_REAL_ROBOT_H
+
+#include <cstring>
+#include "rclcpp/rclcpp.hpp"
+
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+#include "config.h"
+#include "wheel.h"
+#include "arduino_comms.h"
+
+using hardware_interface::return_type;
+
+class DiffDriveArduino : public hardware_interface::SystemInterface
+{
+
+
+public:
+  DiffDriveArduino();
+
+  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+
+
+private:
+
+  Config cfg_;
+  ArduinoComms arduino_;
+
+  Wheel l_wheel_;
+  Wheel r_wheel_;
+
+  rclcpp::Logger logger_;
+
+  std::chrono::time_point<std::chrono::system_clock> time_;
+  
+};
+
+
+#endif // DIFFDRIVE_ARDUINO_REAL_ROBOT_H
