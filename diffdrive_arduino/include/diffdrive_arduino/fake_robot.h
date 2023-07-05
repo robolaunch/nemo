ed60aecfad8a2e856f600c7913e300815027a643
esamet <samete354@gmail.com>
2023-07-05 13:08:08 +0300

    added .h files

---
+++ diffdrive_arduino/include/diffdrive_arduino/fake_robot.h
@@ -0,0 +1,55 @@
+#ifndef DIFFDRIVE_ARDUINO_FAKE_ROBOT_H
+#define DIFFDRIVE_ARDUINO_FAKE_ROBOT_H
+
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+#include "config.h"
+#include "wheel.h"
+
+using hardware_interface::return_type;
+
+class FakeRobot : public hardware_interface::SystemInterface
+{
+
+
+public:
+  FakeRobot();
+
+  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+
+private:
+
+  Config cfg_;
+
+  Wheel l_wheel_;
+  Wheel r_wheel_;
+
+  rclcpp::Logger logger_;
+
+  std::chrono::time_point<std::chrono::system_clock> time_;
+  
+};
+
+#endif // DIFFDRIVE_ARDUINO_FAKE_ROBOT_H
