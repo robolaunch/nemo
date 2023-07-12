b074fcdfc6aa0c7bb707aa3e294ab31608dfe806
esamet <samete354@gmail.com>
2023-07-12 23:43:53 +0300

    bno055 imu update

--- articubot_one/launch/navigation_launch.py
+++ articubot_one/launch/navigation_launch.py
@@ -78,12 +78,12 @@ def generate_launch_description():
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='true',
+        default_value='false',
         description='Use simulation (Gazebo) clock if true')
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value=os.path.join(bringup_dir, 'config', 'nav2_params.yaml'),
+        default_value=os.path.join(bringup_dir, 'config', 'nav2_params_dev.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_autostart_cmd = DeclareLaunchArgument(
