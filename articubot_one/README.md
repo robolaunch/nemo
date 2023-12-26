5371629aec7bab8263c9ec5fa81457d6a6892a67
esamet <samete354@gmail.com>
2023-12-26 16:28:57 +0300

    new branch

--- articubot_one/README.md
+++ articubot_one/README.md
@@ -2,6 +2,6 @@
 
 <h3> Publish static transform for navigation</h3>
 
-```ros2 run tf2_ros static_transform_publisher 0 0 0 0.0 0.0 0.0 odom base_link_frame```
+```ros2 run tf2_ros static_transform_publisher 0 0 0 0.0 0.0 0.0 odom base_link```
 
 
--- articubot_one/launch/navigation_launch.py
+++ articubot_one/launch/navigation_launch.py
@@ -79,7 +79,7 @@ def generate_launch_description():
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='true',
+        default_value='false',
         description='Use simulation (Gazebo) clock if true')
 
     declare_params_file_cmd = DeclareLaunchArgument(
